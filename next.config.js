9d6a1e9100c5bf9f785444e2ca2099ec59aa6c00
iaman221b <iaman221b@gmail.com>
2022-05-23 16:30:37 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,18 +1,53 @@
-//empty config
-// module.exports = {};
+/** @type {import('next').NextConfig} */
+/* eslint-disable */
+const path = require('path')
+const NextImages = require('next-images')
 
-// config for webpack
-// module.exports = {
-//   webpack(config) {
-//     config.module.rules.push({
-//       test: /\.svg$/,
-//       use: ["@svgr/webpack"]
-//     });
+const nextConfig = {
+  reactStrictMode: true,
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
+  images: {
+    domains: ['dev-next.msme.jswone.in', 'images.ctfassets.net', 'th.bing.com'],
+  },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    })
 
-//     return config;
-//   }
-// };
+    return config
+  },
+  eslint: {
+    dirs: [
+      'pages',
+      'components',
+      'lib',
+      'core',
+      'modules',
+      'shared',
+      'utility',
+    ],
+  },
 
-// config for next-images
-const withImages = require("next-images");
-module.exports = withImages();
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'X-DNS-Prefetch-Control',
+            value: 'on',
+          },
+          {
+            key: 'X-XSS-Protection',
+            value: '1; mode=block',
+          },
+        ],
+      },
+    ]
+  },
+}
+
+module.exports = nextConfig
